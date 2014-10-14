287e393e2662e86509f6e1408eae586a5b6d00be
Peter Wik <peter.wik@affecto.com>
2014-10-14 11:54:05 +0300

    my first commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse=NULL
+  set <- function(y){ ##Setting the matrix
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function(){ ##getting the matrix
+    x 
+  }
+  getInverse <- function(){ ##get the inverse
+    inverse
+  }
+  setInverse <- function(newinverse){ ##set the inverse
+    inverse <<- newinverse
+  }
+  list(set=set,get=get,getInverse=getInverse,setInverse=setInverse) ##expose functions as a list
 }
 
 
@@ -12,4 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse() ##try to get inverse from cache
+  if (is.null(inverse)){
+    message("No inverse in cache. Calculating...")
+    inverse <- solve(x$get()) ##calculate inverse 
+    x$setInverse(inverse) ##set inverse to cache
+  }
+  inverse ##return inverse
 }
